780022c3bb24973b20c21263f0a3b525f1489769
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-31 14:41:30 -0700

    link redux, create store and provider

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,9 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from '../reducers/PeopleReducer';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,15 +19,24 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+// we are just passing all reducers here, which are declared as an import at the top
+//example the people reducer;
+//then we will need to create a provider and pass the store inside the object;
+//essentially wrapping redux inside the component.   <Provider store={store}> etc..
+const store = createStore(reducers);
+
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
+      <Provider store={store}>
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
+      </Provider>
     );
   }
 }
