346a3b44cc3cae41e167a7397466c467588713c8
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-31 14:05:26 -0700

    initial files

---
+++ src/reducer/PeopleReducer.js
@@ -0,0 +1,10 @@
+import people from './people.json'
+
+//create a variable initialState with object people
+const initialState = {
+    people,
+}
+
+
+//actions calls the reducer
+//reducer changes the state which renders on the component
