8a8ae156b46e7a15e1b2a05ef24b59433d86119f
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-31 14:17:05 -0700

    add people reducer

--- src/reducer/PeopleReducer.js
+++ src/reducer/PeopleReducer.js
@@ -1,3 +1,8 @@
+//Note
+//actions calls the reducer
+//reducer changes the state which renders on the components 
+
+
 import people from './people.json'
 
 //create a variable initialState with object people
@@ -5,6 +10,16 @@ const initialState = {
     people,
 }
 
+// create fuction which will pass as the state, the intial state and action.
+// inside the function we use a switch statement and returns a new state.
+// How redux works: once we call from the application the action, it will be an action type.
+// The action type will be passed through the reducer with the initialState
+// base on the action type, it will return a new state.
+export default (state = initialState, actions) => {
+    switch (actions.type) {
+        default:
+            return state;
+    }
+}
 
-//actions calls the reducer
-//reducer changes the state which renders on the component
+//now we will create the people.json file
