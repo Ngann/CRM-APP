076d4ffa5a02a205f9e3b22d88b13bab766ce4f3
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-31 18:43:03 -0700

    add PeopleItem component with image and themes

---
+++ src/components/PeopleItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { connect } from 'react-redux';
+import { getTheme } from 'react-native-material-kit';
+import * as actions from '../actions';
+
+const theme = getTheme();
+
+const styles = StyleSheet.create({
+    card: {
+        marginTop: 20,
+    },
+    title: {
+        top: 20,
+        left: 80,
+        fontSize: 24,
+    },
+    image: {
+        height: 100,
+    },
+    action: {
+        backgroundColor: 'black',
+        color: 'white',
+    },
+    icon: {
+        position: 'absolute',
+        top: 15,
+        left: 0,
+        color: 'white',
+        backgroundColor: 'rgba(255,255,255,0)',
+    },
+});
+
+
+const PeopleItem = (props) => {
+    return (
+        <View style={[theme.cardStyle, styles.card]}>
+            <Image 
+                source={require('../images/background.jpg')}
+                style={[theme.cardImageStyle, styles.image]}
+            />
+            <Text 
+                name={'user'}
+                size={100}
+                style={styles.icon}
+            />
+            <Text style={[theme.cardTitleStyle, styles.title]}>{props.people.firstName} {props.people.lastName}</Text>
+            <Text style={[theme.cardActionStyle, styles.action]}>{props.people.company}</Text>
+        </View>
+    )
+}
+
+export default connect(null, actions)(PeopleItem);
